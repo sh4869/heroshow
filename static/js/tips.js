2bad93c6d80331abbd23d7b3d926031e3463de35
sh4869 <nobuk4869@gmail.com>
2017-12-11 11:17:28 +0900

    Fix tippy.js

--- static/js/tips.js
+++ static/js/tips.js
@@ -1,9 +1,17 @@
 var comments = document.querySelectorAll(".footnote-ref");
-var i = 0;
-for(i = 0;i < comments.length;i++){
+var footnotes = document.querySelectorAll(".footnotes li");
+for(var i = 0;i < comments.length;i++){
+      // comment の方のidをmodify
+      comments[i].id = comments[i].id + i.toString();
+      comments[i].querySelector("a").href = comments[i].querySelector("a").href + i.toString()
+      // footnote の方をmodify
+      footnotes[i].id = footnotes[i].id + i.toString();
+      footnotes[i].querySelector("a").href = footnotes[i].querySelector("a").href + i.toString()
+
       var id = comments[i].id.split("fnref:").pop();
       var text = document.querySelector("#fn\\:" + id).innerText.replace("[return]","");
       comments[i].title = text;
+      
       tippy(comments[i],{
             arrow: 'small',
             stickyDuration: 100,
