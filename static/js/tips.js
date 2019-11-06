68e41441b20e42383a37d5572a5c9f86c6daf657
sh4869 <nobuk4869@gmail.com>
2019-11-06 15:00:36 +0900

    load lazy fix tips.js

--- static/js/tips.js
+++ static/js/tips.js
@@ -1,21 +1,27 @@
-var comments = document.querySelectorAll(".footnote-ref");
-var footnotes = document.querySelectorAll(".footnotes li");
-for(var i = 0;i < comments.length;i++){
-      // comment の方のidをmodify
-      comments[i].id = comments[i].id + i.toString();
-      comments[i].querySelector("a").href = comments[i].querySelector("a").href + i.toString()
-      // footnote の方をmodify
-      footnotes[i].id = footnotes[i].id + i.toString();
-      footnotes[i].querySelector("a").href = footnotes[i].querySelector("a").href + i.toString()
+window.addEventListener("Load", function() {
+  var comments = document.querySelectorAll(".footnote-ref");
+  var footnotes = document.querySelectorAll(".footnotes li");
+  for (var i = 0; i < comments.length; i++) {
+    // comment の方のidをmodify
+    comments[i].id = comments[i].id + i.toString();
+    comments[i].querySelector("a").href =
+      comments[i].querySelector("a").href + i.toString();
+    // footnote の方をmodify
+    footnotes[i].id = footnotes[i].id + i.toString();
+    footnotes[i].querySelector("a").href =
+      footnotes[i].querySelector("a").href + i.toString();
 
-      var id = comments[i].id.split("fnref:").pop();
-      var text = document.querySelector("#fn\\:" + id).innerText.replace("[return]","");
-      comments[i].title = text;
-      
-      tippy(comments[i],{
-            arrow: 'small',
-            stickyDuration: 100,
-            distance: 9,
-            size: "small"
-      })
-}
+    var id = comments[i].id.split("fnref:").pop();
+    var text = document
+      .querySelector("#fn\\:" + id)
+      .innerText.replace("[return]", "");
+    comments[i].title = text;
+
+    tippy(comments[i], {
+      arrow: "small",
+      stickyDuration: 100,
+      distance: 9,
+      size: "small"
+    });
+  }
+});
